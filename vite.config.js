3bc4225d0e8a6ec78b250d754869e458a8ecd5e4
Nse13 <gobinomanuel@outlook.it>
2025-04-12 13:18:28 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  root: './',
   plugins: [react()],
-  base: './', // ✅ aggiungi questa riga per farlo funzionare su Vercel
+  base: './',
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: path.resolve(__dirname, 'index.html'),
+    },
+  },
 });
