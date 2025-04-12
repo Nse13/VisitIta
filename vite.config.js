2d0a16ce6f5a4ccb6a4103d4f86aa4f1d0382d16
Nse13 <gobinomanuel@outlook.it>
2025-04-12 18:33:19 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
 
 export default defineConfig({
-  root: './',
   plugins: [react()],
-  base: './',
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      input: path.resolve(__dirname, 'index.html'),
-    },
-  },
 });
