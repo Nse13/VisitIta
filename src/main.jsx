738a6dc42917771770750bb4e359c628bd27787e
Nse13 <gobinomanuel@outlook.it>
2025-04-12 10:32:21 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import VisitItaApp from "./components/VisitItaApp";
-import regionsData from "./data/regionsData";
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './components/App.jsx'
+import './index.css'
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <VisitItaApp regionsData={regionsData} />
-  </React.StrictMode>
-);
+    <App />
+  </React.StrictMode>,
+)
